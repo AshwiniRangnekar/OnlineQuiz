e933af3d3ec772dc39062d17eca0a11f7773c059
Parul Jain <32247098+paruljain02@users.noreply.github.com>
2018-11-02 00:33:44 +0530

    Add files via upload
    Functions made

--- Smart-Contract-OnlineQuiz/contracts/OnlineQuiz.sol
+++ Smart-Contract-OnlineQuiz/contracts/OnlineQuiz.sol
@@ -1,7 +1,8 @@
-pragma solidity ^0.4.22;
-
-contract OnlineQuiz{
-	struct Question{
+pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
+contract OnlineQuiz {
+    
+    struct Question{
         string QUES;
         string ANS;
         uint count;
@@ -18,10 +19,11 @@ contract OnlineQuiz{
     uint till_noques;
     
     uint time;
+    
     //uint[] countarray;
     
     address[] players;
-
+    
     mapping(address => uint) players_mapping;
     mapping(address => uint) players_Ans_mapping;
     mapping(uint => address[]) ques_player_mapping;
@@ -53,6 +55,8 @@ contract OnlineQuiz{
         require ( wallet >= prfee, "INSUFFCIENT BALANCE");
         _;
     }
+
+
     constructor(uint fee, uint n, uint noq) 
     public
     {
@@ -65,4 +69,93 @@ contract OnlineQuiz{
         noques = noq;
         till_noques = 0;
     }
+    
+    function addQuestion(string ques, string ans)
+    public is_organiser()
+    {
+        
+        if(till_noques < noques){
+            till_noques +=1;
+            questions.push(Question({
+                QUES: ques,
+                ANS: ans,
+                count: 0
+            }));
+        }
+    }
+    
+    
+    function registerParticipants(uint wallet) public payable
+    not_organiser()
+    check_player_balance(wallet, pfee)
+    check_max_players()
+    {
+        tfee += pfee;
+        
+        players_mapping[msg.sender] = 1;
+        players_balance_mapping[msg.sender] = wallet - pfee;
+        //calculateCorrectAnswer(_answer1,_answer2,_answer3,_answer4);
+        maxplayer -= 1;
+        players.push(msg.sender);
+    }
+    
+    function compare_answers (string given, string answered)
+    public returns (bool)
+    {
+        bytes memory given1 = bytes(given);
+        bytes memory answered1 = bytes(answered);
+        return keccak256(given1) == keccak256(answered1);
+    }
+    
+    function play(string[] answers) public registered(msg.sender)
+    {
+        //uint correct_ans = 0;
+        for (uint i = 0; i< questions.length; i++)
+        {
+            if(compare_answers(questions[i].ANS, answers[i]))
+            {
+                
+                questions[i].count += 1;
+                ques_player_mapping[i].push(msg.sender);
+                //players_Ans_mapping[msg.sender] = i;
+                //correct_ans ++ ;
+            }
+        }
+        //players_Ans_mapping[msg.sender] = correct_ans;
+        
+    }
+    
+    function Payto_winner()
+    public
+    is_organiser()
+    {
+
+        uint pay;
+        
+        for(uint i = 0;i < questions.length;i++){
+           
+           for(uint j = 0;j< ques_player_mapping[i].length;j++)
+           {
+               
+            pay = ((3*tfee)/16)/ques_player_mapping[i].length;
+            players_balance_mapping[ques_player_mapping[i][j]] += pay; 
+            
+            tfee -= pay; 
+           }
+           
+        }
+       
+    }
+    
+    function viewbalance_palyer() public not_organiser() view returns (address participant, uint balance) {
+        
+        return (msg.sender , players_balance_mapping[msg.sender]);
+    }
+    
+    function viewbalance_org() public is_organiser() view returns (uint) {
+        
+        return tfee;
+    }
+    
+    
 }
