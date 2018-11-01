e64cfd19b9050436c193e15ccdacb61b8be8939f
Rahil Sheth <36309845+Rahil-17@users.noreply.github.com>
2018-11-01 22:41:30 +0530

    Add modifiers in smart contract

--- Smart-Contract-OnlineQuiz/contracts/OnlineQuiz.sol
+++ Smart-Contract-OnlineQuiz/contracts/OnlineQuiz.sol
@@ -21,6 +21,38 @@ contract OnlineQuiz{
     //uint[] countarray;
     
     address[] players;
+
+    mapping(address => uint) players_mapping;
+    mapping(address => uint) players_Ans_mapping;
+    mapping(uint => address[]) ques_player_mapping;
+    mapping(address => uint) players_balance_mapping;
+
+
+    modifier is_organiser(){
+        require(msg.sender==organiser,"I am not an ORGANISER");
+        _;
+    }
+    
+     modifier not_organiser(){
+        require(msg.sender!=organiser,"I am an ORGANISER");
+        _;
+    }
+   
+    modifier check_max_players(){
+        require(maxplayer > 0,"LIMIT REACHED");
+        _;
+    }
+    
+    modifier registered(address player){
+        require(players_mapping[player]==0,"You are registered");
+        _;
+    }
+
+    modifier check_player_balance(uint wallet , uint prfee)
+    {
+        require ( wallet >= prfee, "INSUFFCIENT BALANCE");
+        _;
+    }
     constructor(uint fee, uint n, uint noq) 
     public
     {
